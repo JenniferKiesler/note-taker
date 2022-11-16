53a14eafc49df04ab17fc2cbf73e245e8848135b
JKies <jennyhawes24@gmail.com>
2022-11-15 23:41:43 -0600

    adds comments to server.js, api.js, and views.js

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,22 @@ const path = require('path')
 const fs = require('fs')
 const shortid = require('shortid')
 
+// GET Route that reads db.json and returns all saved notes as JSON
 router.get('/notes', (req, res) => {
+    // reads db.json file
     fs.readFile(path.join(__dirname, "..", "db", "db.json"), 'utf-8', function(err, data) {
         if (err) {
             res.status(500).json(err)
             return
         }
         const json = JSON.parse(data)
+        
+        // responds with parsed data
         res.json(json)
     })
 })
 
+// POST Route that adds a new note to db.json and returns it to the client
 router.post('/notes', (req, res) => {
     const {title, text} = req.body
     
@@ -23,10 +28,12 @@ router.post('/notes', (req, res) => {
     }
 
     const newNote = {
+        // creates unique id
         id: shortid.generate(),
         ...req.body
     }
 
+    // reads contents of db.json
     fs.readFile(path.join(__dirname, "..", "db", "db.json"), 'utf-8', function(err, data) {
         if (err) {
             res.status(500).json(err)
@@ -34,8 +41,11 @@ router.post('/notes', (req, res) => {
         }
 
         const notes = JSON.parse(data)
+        
+        // adds new note to the db.json file
         notes.push(newNote)
         
+        // stringify notes array and saves file
         fs.writeFile(path.join(__dirname, "..", "db", "db.json"), JSON.stringify(notes), function(err) {
             if (err) {
                 res.status(500).json(err)
@@ -47,6 +57,8 @@ router.post('/notes', (req, res) => {
     })
 })
 
+
+// DELETE Route that uses the unique id to delete the corresponding note
 router.delete('/notes/:id', (req, res) => {
    const id = req.params.id
 
@@ -55,10 +67,14 @@ router.delete('/notes/:id', (req, res) => {
     return
   }
 
+  // reads all notes from db.json
   fs.readFile(path.join(__dirname, "..", "db", "db.json"), "utf-8", function(err, data) {
     const notes = JSON.parse(data)
+    
+    // modify notes array so it doesn't include the note with the given id
     const updatedNotes = notes.filter(note => id != note.id)
 
+    // rewrites the notes to db.json
     fs.writeFile(path.join(__dirname, "..", "db", "db.json"), JSON.stringify(updatedNotes), function(err) {
         if (err) {
             res.status(500).json(err)
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Unblock static folder
 app.use(express.static("public"))
 app.use(express.json())
 
 app.use(routes)
 
+// listens for connections on the specified host and port
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
 })
